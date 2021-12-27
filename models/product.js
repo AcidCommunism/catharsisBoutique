7148509e6b1fe9121b5d593c286c9747f5deed6f
AcidCommunism <max.zamota@gmail.com>
2021-12-27 05:45:23 +0300

    Add all current stuff

---
+++ models/product.js
@@ -0,0 +1,32 @@
+const db = require("../util/database");
+
+const Cart = require("./cart");
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    return db.execute(
+      "INSERT INTO PRODUCTS (title, price, description, imageUrl) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.description, this.imageUrl]
+    );
+  }
+
+  static deleteById(id) {
+    return db.execute(`DELETE FROM PRODUCTS WHERE id=${id}`);
+  }
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM PRODUCTS");
+  }
+
+  static findById(id) {
+    return db.execute("SELECT * FROM PRODUCTS WHERE id = ?", [id]);
+  }
+};
