9ace6160cb12e2fe704f5dd11cf5e4377ebd223d
AcidCommunism <max.zamota@gmail.com>
2022-11-26 19:11:29 +0400

    add auth check

---
+++ src/server/middlewares/auth-check.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+export function authCheck(
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+) {
+    if (!request.session.isAuthenticated) {
+        return response.redirect('/sign-in');
+    }
+    next();
+}
